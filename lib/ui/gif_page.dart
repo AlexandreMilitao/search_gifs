201b6b62fdd830a5d28d33bf7f8872e5306e0fe6
AleLevi <ale.levi@hotmail.com>
2022-05-20 10:33:32 -0300

    onLongPress_and_share(bottomshare)

--- lib/ui/gif_page.dart
+++ lib/ui/gif_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_field, prefer_const_constructors_in_immutables, use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class GifPage extends StatelessWidget {
   final Map _gifData;
@@ -13,10 +14,22 @@ class GifPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(_gifData["title"]),
         backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                Share.share(
+                  _gifData["images"]["fixed_height"]["url"],
+                );
+              },
+              icon: Icon(Icons.share))
+        ],
       ),
       backgroundColor: Colors.black,
       body: Center(
-          child: Image.network(_gifData["images"]["fixed_height"]["url"])),
+        child: Image.network(
+          _gifData["images"]["fixed_height"]["url"],
+        ),
+      ),
     );
   }
 }
